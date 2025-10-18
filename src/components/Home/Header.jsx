6548e2df0b1db3858ab2ea8740a6c1430a80472e
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-10-18 12:04:15 -0300

    corregimos responsive del home1

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -77,7 +77,7 @@ function Header() {
                                 </a>
                             </div>
                         </div>
-                        <div className="absolute inset-y-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-sm sm:top-[calc(100%-30rem)]">
+                        <div className="absolute inset-y-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-sm sm:top-[calc(100%-30rem)] xs:hidden">
                             <svg
                                 className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                                 viewBox="0 0 1155 678"
